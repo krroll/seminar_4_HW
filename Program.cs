3acaa0bf643fe44bc4321ea281652e88cec084af
Anton <krroll@bk.ru>
2022-12-26 23:44:28 +0800

    4hw finish 25

--- Program.cs
+++ Program.cs
@@ -3,8 +3,18 @@
 // число A в натуральную степень B.
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
+Console.WriteLine("Введите число A ");
+int a = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine("Введите число B ");
+int b = Convert.ToInt32 (Console.ReadLine());
 
+int GetSqt ()
+{
+    int numb = Convert.ToInt32 (Math.Pow(a,b));
+    return numb;
+}
 
+Console.WriteLine("A в степени B = " + GetSqt());
 
 
 // --------------------------------------

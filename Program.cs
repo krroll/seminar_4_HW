25ace1fa8d282ddb34f178404fd6160a2c2fc98c
Anton <krroll@bk.ru>
2022-12-27 00:51:00 +0800

    task 27 done

--- Program.cs
+++ Program.cs
@@ -3,18 +3,19 @@
 // число A в натуральную степень B.
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
-Console.WriteLine("Введите число A ");
-int a = Convert.ToInt32 (Console.ReadLine());
-Console.WriteLine("Введите число B ");
-int b = Convert.ToInt32 (Console.ReadLine());
 
-int GetSqt ()
-{
-    int numb = Convert.ToInt32 (Math.Pow(a,b));
-    return numb;
-}
+// Console.WriteLine("Введите число A ");
+// int a = Convert.ToInt32 (Console.ReadLine());
+// Console.WriteLine("Введите число B ");
+// int b = Convert.ToInt32 (Console.ReadLine());
 
-Console.WriteLine("A в степени B = " + GetSqt());
+// int GetSqt ()
+// {
+//     int numb = Convert.ToInt32 (Math.Pow(a,b));
+//     return numb;
+// }
+
+// Console.WriteLine("A в степени B = " + GetSqt());
 
 
 // --------------------------------------
@@ -25,9 +26,22 @@ Console.WriteLine("A в степени B = " + GetSqt());
 // 82 -> 10
 // 9012 -> 12
 
+// Console.WriteLine("Введите число ");
+// int num =  Convert.ToInt32 (Console.ReadLine());
 
-
-
+// int GetSum()
+// {
+//     int sum = 0;
+//     int ostatok = 0;
+//     while (num != 0)
+//     {
+//         ostatok = num % 10;             // получаем остаток от числа, то есть последний символ числа
+//         num = num/10;                   // Удаляем последнее число в цепочке 
+//         sum = sum + ostatok;            //суммируем полученный последний символ числа 
+//     }
+//     return sum;
+// }
+// Console.WriteLine("Сумма цифр = " + GetSum());
 
 
 // ------------------------------------------
